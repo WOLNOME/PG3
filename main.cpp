1f4bd2451434f84299472ff3ce1208696403fdcd
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-09-30 11:04:49 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <iostream>
+#include <Windows.h>
 
 int main() {
-
+	SetConsoleCP(65001);
+	char str[] = "ある日　森の中　くまさんに　出会った　花咲く森の道　くまさんに　出会った";
+	printf("%s", str);
 
 
 	return 0;
