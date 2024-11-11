953621eb7acc7c5778e56c1a143434d65ed6ebc9
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-11-11 11:34:05 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <iostream>
 
-int main() {
+class Weapon {
+public:
+    Weapon() = default;
+    virtual ~Weapon() = default;
 
+    // 純粋仮想関数
+    virtual void Attack() = 0;
+
+protected:
+    const char* name;
+};
+
+class Sword : public Weapon {
+public:
+    Sword() { name = "剣"; }
+    void Attack() override {
+        printf("%s で 斬撃！\n",name);
+    }
+};
+
+class Bow : public Weapon {
+public:
+    Bow() { name = "弓"; }
+    void Attack() override {
+        printf("%s で 射撃！\n",name);
+    }
+};
+
+int main() {
+    Weapon* sword = new Sword();
+    Weapon* bow = new Bow();
+
+    sword->Attack();
+    bow->Attack();
+
+    delete sword;
+    delete bow;
     return 0;
 }
