8edf2fe6d98cdced9e2edda9a3416328ab6cdbdc
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-11-11 11:21:47 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,41 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 
-class Weapon {
-public:
-    Weapon() = default;
-    virtual ~Weapon() = default;
-
-    virtual void Attack() = 0;
-
-protected:
-    const char* name;
-};
-
-class Sword : public Weapon {
-public:
-    Sword() { name = "Sword"; }
-    void Attack() override {
-        printf("%s attacks with a slash!\n", name);
-    }
-};
-
-class Bow : public Weapon {
-public:
-    Bow() { name = "Bow"; }
-    void Attack() override {
-        printf("%s shoots an arrow!\n", name);
-    }
-};
-
 int main() {
-    Weapon* sword = new Sword();
-    Weapon* bow = new Bow();
 
-    sword->Attack(); 
-    bow->Attack();  
-
-    delete sword;
-    delete bow;
     return 0;
 }
