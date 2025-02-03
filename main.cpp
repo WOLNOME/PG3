aa4b3357834e17a184e1cd4bb706e5c469e60eb7
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2025-02-03 11:06:15 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,90 +1,32 @@
 #include <stdio.h>
-#include <iostream>
-#include <list>
+#include <string>
+#include <chrono>
+#include <utility>
 
 int main() {
-	//リスト
-	std::list<const char*> yamanotesen;
-	//1970年の駅名を保存
-	yamanotesen.push_back("Tokyo");
-	yamanotesen.push_back("Kanda");
-	yamanotesen.push_back("Akihabara");
-	yamanotesen.push_back("Okachimachi");
-	yamanotesen.push_back("Ueno");
-	yamanotesen.push_back("Uguisudani");
-	yamanotesen.push_back("Nippori");
-	//yamanotesen.push_back("Nishi-Nippori");
-	yamanotesen.push_back("Tabata");
-	yamanotesen.push_back("Komagome");
-	yamanotesen.push_back("Sugamo");
-	yamanotesen.push_back("Otsuka");
-	yamanotesen.push_back("Ikebukuro");
-	yamanotesen.push_back("Mejiro");
-	yamanotesen.push_back("Takadanobaba");
-	yamanotesen.push_back("Shin-Okubo");
-	yamanotesen.push_back("Shinjuku");
-	yamanotesen.push_back("Yoyogi");
-	yamanotesen.push_back("Harajuku");
-	yamanotesen.push_back("Shibuya");
-	yamanotesen.push_back("Ebisu");
-	yamanotesen.push_back("Meguro");
-	yamanotesen.push_back("Gotanda");
-	yamanotesen.push_back("Osaki");
-	yamanotesen.push_back("Shinagawa");
-	//yamanotesen.push_back("Takanawa Gateway");
-	yamanotesen.push_back("Tamachi");
-	yamanotesen.push_back("Hamamatsucho");
-	yamanotesen.push_back("Shimbashi");
-	yamanotesen.push_back("Yurakucho");
-	//表示
-	printf("1970\n");
-	for (std::list<const char*>::iterator it = yamanotesen.begin(); it != yamanotesen.end(); ++it) {
-		if (*it == "Yurakucho") {
-			printf("%s\n", *it);
-		}
-		else {
-			printf("%s,", *it);
-		}
-		
-	}
-	//西日暮里を挿入
-	for (std::list<const char*>::iterator it = yamanotesen.begin(); it != yamanotesen.end(); ++it) {
-		if (*it == "Tabata") {
-			it = yamanotesen.insert(it, "Nishi-Nippori");
-			++it;  // 挿入後の位置を調整
-		}
-	}
-	//表示
-	printf("2019\n");
-	for (std::list<const char*>::iterator it = yamanotesen.begin(); it != yamanotesen.end(); ++it) {
-		if (*it == "Yurakucho") {
-			printf("%s\n", *it);
-		}
-		else {
-			printf("%s,", *it);
-		}
-	}
-	//高輪ゲートウェイを挿入
-	for (std::list<const char*>::iterator it = yamanotesen.begin(); it != yamanotesen.end(); ++it) {
-		if (*it == "Tamachi") {
-			it = yamanotesen.insert(it, "Takanawa Gateway");
-			++it;  // 挿入後の位置を調整
-		}
-	}
-	//表示
-	printf("2022\n");
-	for (std::list<const char*>::iterator it = yamanotesen.begin(); it != yamanotesen.end(); ++it) {
-		if (*it == "Yurakucho") {
-			printf("%s\n", *it);
-		}
-		else {
-			printf("%s,", *it);
-		}
-	}
+	//元となる文字列
+	std::string a(100000, 'a');
 
+	printf("100,000文字を移動とコピーで比較しました。\n");
 
-
-
+	//コピー計測開始
+	auto copyStart = std::chrono::high_resolution_clock::now();
+	//コピー処理
+	std::string b = a;
+	//コピー計測終了
+	auto copyEnd = std::chrono::high_resolution_clock::now();
+	//計測時間を表示
+	auto copyDuration = std::chrono::duration_cast<std::chrono::microseconds>(copyEnd - copyStart);
+	printf("コピー : %dμs\n", (int)copyDuration.count());
+	//移動計測開始
+	auto moveStart = std::chrono::high_resolution_clock::now();
+	//移動処理
+	std::string c = std::move(a);
+	//移動計測終了
+	auto moveEnd = std::chrono::high_resolution_clock::now();
+	//計測時間を表示
+	auto moveDuration = std::chrono::duration_cast<std::chrono::microseconds>(moveEnd - moveStart);
+	printf("移動 : %dμs\n", (int)moveDuration.count());
 
 
 	return 0;
