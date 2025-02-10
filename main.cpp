1e7affc8a39e179cf1c9c276fcba764b6df1e92a
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2025-02-10 14:47:52 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,59 +1,27 @@
 #include <stdio.h>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <cassert>
+#include <functional>
+#include <thread>
 
 using namespace std;
 
 int main() {
-    // ファイル名
-    string filename = "PG3_2024_03_02.txt";
-    ifstream file(filename);
+	function<void()> threadOne = [=]() {
+		printf("thread1\n");
+		};
+	function<void()> threadTwo = [=]() {
+		printf("thread2\n");
+		};
+	function<void()> threadThree = [=]() {
+		printf("thread3\n");
+		};
 
-    if (!file) {
-        assert(0 && "ファイルを開けませんでした");
-    }
+	thread th1(threadOne);
+	th1.join();
+	thread th2(threadTwo);
+	th2.join();
+	thread th3(threadThree);
+	th3.join();
 
-    string line;
-    vector<string> emails;
 
-    // ファイルから1行読み込む
-    if (getline(file, line)) {
-        // 角括弧を削除
-        if (!line.empty() && line.front() == '[') line.erase(line.begin());
-        if (!line.empty() && line.back() == ']') line.pop_back();
-
-        stringstream ss(line);
-        string email;
-
-        // カンマ(,)区切りで分割
-        while (getline(ss, email, ',')) {
-            // 不要な空白とダブルクォートを削除
-            email.erase(remove(email.begin(), email.end(), ' '), email.end());
-            email.erase(remove(email.begin(), email.end(), '\"'), email.end());
-            emails.push_back(email);
-        }
-    }
-
-    // ファイルを閉じる
-    file.close();
-
-    // 一旦出力
-    printf("一旦全要素を出力する\n");
-    for (const string& email : emails) {
-        printf("%s\n", email.c_str());
-    }
-
-    // 昇順に並び替え
-    sort(emails.begin(), emails.end());
-    printf("昇順に並び替え\n");
-    for (const string& email : emails) {
-        printf("%s\n", email.c_str());
-    }
-
-    return 0;
+	return 0;
 }
